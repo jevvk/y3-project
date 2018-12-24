fc0304caf772c2da2a2505b16f719561d731f2be
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-24 23:45:21 +0000

    Add arguments logic to main file

--- y3p/__main__.py
+++ y3p/__main__.py
@@ -1,11 +1,26 @@
-# for each frame
-  # camera_positions = []
-  # for each camera
-    # detect players
-    # perform team classification (A, B, unknown)
-    # filter out unknown class
-    # map each detection to the position relative to the court
-    # append each position to camera_possitions
-  # aggregate camera_positions into positions
-# calculate statistics from positions
-# write results
+import argparse
+import yaml
+
+from y3p.logic import main
+
+parser = argparse.ArgumentParser(description='Short sample app')
+
+parser.add_argument('config')
+parser.add_argument('--model', choices = ['mrcnn'], default = 'mrcnn')
+
+args = parser.parse_args()
+
+detector = None
+config = None
+
+if args.model == 'mrcnn':
+  from y3p.detector.maskrcnn import MaskRCNNDetector
+  detector = MaskRCNNDetector()
+else:
+  # TODO yolo detector
+  pass
+
+with open(args.config, 'r') as stream:
+  config = yaml.load(stream)
+
+  main(config, detector)
