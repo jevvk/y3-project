928a04a3f9ced571f2a0614b9ecfe89f58275189
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-24 23:46:10 +0000

    Add empty files

---
+++ y3p/player/classification.py
@@ -0,0 +1,6 @@
+"""
+Given a list of images and masks, return a number for each player. Number
+0 represents no team, 1 represents team A and 2 represents team B.
+"""
+def classify(players, masks):
+  pass
---
+++ y3p/player/feature.py
@@ -0,0 +1,5 @@
+"""
+Given an image of the player and a mask, return the features of the player.
+"""
+def compute(player, mask):
+  pass
