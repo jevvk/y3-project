a3eac5e05ee97014a1f1c7326956305e58a29d0f
LiNoimi Semain <iplay.addicted@gmail.com>
2019-02-24 20:25:49 +0000

    Add confidence to position

--- y3p/player/__init__.py
+++ y3p/player/__init__.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 
 class Player:
@@ -7,17 +8,31 @@ class Player:
     self.height = detection[2]
     self.width = detection[3]
     self.image = detection[4]
-    self.mask = detection[5]
+    # self.mask = detection[5]
+    self.mask = None
     self.camera = camera
 
-    self._set_feet_position()
+    self._calculate_feet_position()
 
-  def _set_feet_position(self):
+  def _calculate_feet_position(self):
+    # might do something different here with the mask
     self.feet_x = int(self.x + self.width / 2)
     self.feet_y = int(self.y + self.height)
 
   def get_position(self, field):
     feet = [self.feet_x, self.feet_y]
-    position = field.get_position(self.camera, feet)
+    position = field.get_position(self.camera, feet, use_homography=False)
+    camera = field.get_camera(self.camera)
+    T = np.dot(camera.R.T, -camera.T)
+    diff = position + np.array([T[0], T[2]])
+    diff /= field.size
+    confidence = 1 + np.sum(diff * diff)
+    # confidence = 1 / dist(diff)
 
-    return position
+    return position, confidence
+
+def norm(v):
+  return v / dist(v)
+
+def dist(v):
+  return math.sqrt(np.sum(v*v))
